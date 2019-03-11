a1a36f408bb42c76ac9954676e9c144f9b626acd
Sakenzhassulan <jasulan.008@mail.ru>
2019-03-11 19:16:19 +0600

    Add Week3

---
+++ Week3/Task1/Program.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_1
+{
+    class Layer   // Создаю класс
+    {
+        public FileSystemInfo[] Content
+        {
+            get;
+            set;
+        }
+
+        int selectedItem;
+
+        public int SelectedItem
+        {
+            get
+            {
+                return selectedItem;
+            }
+            set
+            {
+                if (value < 0)    //   контролирую кнопки 
+                {
+                    selectedItem = Content.Length - 1;
+                }
+                else if (value >= Content.Length)
+                {
+                    selectedItem = 0;
+                }
+                else
+                {
+                    selectedItem = value;
+                }
+            }
+        }
+
+        public void Draw()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Clear();
+            for (int i = 0; i < Content.Length; ++i)
+            {
+                if (i == SelectedItem)
+                {
+                    Console.BackgroundColor = ConsoleColor.Red;        // Функция которая контролирует цвета файлов и папок
+                }
+                else
+                {
+                    Console.BackgroundColor = ConsoleColor.Black;
+                    if (Content[i].GetType() == typeof(DirectoryInfo))
+                        Console.ForegroundColor = ConsoleColor.White;
+                    else
+                        Console.ForegroundColor = ConsoleColor.Yellow;
+                }
+                Console.WriteLine((i + 1) + ". " + Content[i].Name);
+            }
+        }
+    }
+
+    enum FarMode
+    {
+        FileView,
+        DirectoryView
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DirectoryInfo root = new DirectoryInfo(@"D:\c++");    //Присваиваю путь
+            Stack<Layer> history = new Stack<Layer>();
+            FarMode farMode = FarMode.DirectoryView;
+
+            history.Push(
+                new Layer
+                {
+                    Content = root.GetFileSystemInfos(),     // Делаю экземпляр в котором аргумент Content and FarMode
+                    SelectedItem = 0
+                });
+
+            while (true)
+            {
+                if (farMode == FarMode.DirectoryView)
+                {
+                    history.Peek().Draw();     // Вызываю функцию 
+                }
+                ConsoleKeyInfo consoleKeyInfo = Console.ReadKey();
+                switch (consoleKeyInfo.Key)
+                {
+                    case ConsoleKey.F2:
+                        Console.BackgroundColor = ConsoleColor.Black;
+                        Console.Clear();
+                        string name = Console.ReadLine();
+                        int x3 = history.Peek().SelectedItem;
+                        FileSystemInfo fileSystemInfo3 = history.Peek().Content[x3];
+                        if (fileSystemInfo3.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo directoryInfo = fileSystemInfo3 as DirectoryInfo;
+                            Directory.Move(fileSystemInfo3.FullName, directoryInfo.Parent + "/" + name);
+                            history.Peek().Content = directoryInfo.Parent.GetFileSystemInfos();
+                        }
+                        else
+                        {
+                            FileInfo fileInfo = fileSystemInfo3 as FileInfo;
+                            File.Move(fileSystemInfo3.FullName, fileInfo.Directory.FullName + "/" + name);
+                            history.Peek().Content = fileInfo.Directory.GetFileSystemInfos();
+                        }
+
+                        break;
+                    case ConsoleKey.Delete:
+                        int x2 = history.Peek().SelectedItem;
+                        FileSystemInfo fileSystemInfo2 = history.Peek().Content[x2];
+                        history.Peek().SelectedItem--;
+                        if (fileSystemInfo2.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo directoryInfo = fileSystemInfo2 as DirectoryInfo;
+                            Directory.Delete(fileSystemInfo2.FullName, true);
+                            history.Peek().Content = directoryInfo.Parent.GetFileSystemInfos();
+                        }
+                        else
+                        {
+                            FileInfo fileInfo = fileSystemInfo2 as FileInfo;
+                            File.Delete(fileSystemInfo2.FullName);
+                            history.Peek().Content = fileInfo.Directory.GetFileSystemInfos();
+                        }
+
+                        // Результаты нажатых кнопок
+                        break;
+                    case ConsoleKey.UpArrow:
+                        history.Peek().SelectedItem--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        history.Peek().SelectedItem++;
+                        break;
+                    case ConsoleKey.Enter:
+                        int x = history.Peek().SelectedItem;
+                        FileSystemInfo fileSystemInfo = history.Peek().Content[x];
+                        if (fileSystemInfo.GetType() == typeof(DirectoryInfo))
+                        {
+                            DirectoryInfo d = fileSystemInfo as DirectoryInfo;            // Если это папка, показываю содержимое
+                            history.Push(new Layer { Content = d.GetFileSystemInfos(), SelectedItem = 0 });
+                        }
+                        else
+                        {
+                            farMode = FarMode.FileView;
+                            using (FileStream fs = new FileStream(fileSystemInfo.FullName, FileMode.Open, FileAccess.Read))
+                            {
+                                using (StreamReader sr = new StreamReader(fs))
+                                {
+                                    Console.BackgroundColor = ConsoleColor.White;
+                                    Console.ForegroundColor = ConsoleColor.Black;           // если это файл также показываю содержимое
+                                    Console.Clear();
+                                    Console.WriteLine(sr.ReadToEnd());
+                                }
+                            }
+                        }
+                        break;
+                    case ConsoleKey.Backspace:
+                        if (farMode == FarMode.DirectoryView)
+                        {
+                            history.Pop();
+                        }
+                        else if (farMode == FarMode.FileView)
+                        {
+                            farMode = FarMode.DirectoryView;
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
