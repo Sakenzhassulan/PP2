f149c0a99155b9c6645aa78d1b5eb69ac29e8a8a
Sakenzhassulan <jasulan.008@mail.ru>
2019-03-28 02:39:36 +0600

    Add Week6

---
+++ Week6/Snake/Bally.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+    class Bally
+    {
+        public static int Score = 0;
+        public static int PrevS = 0;
+    }
+}
