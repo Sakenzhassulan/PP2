4d146a0e5b14c94ba6cd8d5c9a20761c57b9a0e5
Sakenzhassulan <jasulan.008@mail.ru>
2019-03-28 21:13:49 +0600

    jffffad

---
+++ Week7/Task1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ThreadForConsoleInput
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] srr = new string[4];
+            srr[0] = "Zhassik";
+            srr[1] = "Zhassulan";
+            srr[2] = "Zhass";
+            Thread[] arr = new Thread[3];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = new Thread(func);
+                arr[i].Name = srr[i];
+                arr[i].Start();
+            }
+        }
+        static void func()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name);
+                //Thread.Sleep(0);
+            }
+            //Console.WriteLine(Thread.CurrentThread.Name + " завершился"); 
+        }
+    }
+}
---
+++ Week7/Task2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace Task2
+{
+    class Program
+    {
+        class Mythread
+        {
+            Thread threadfield;
+            public Mythread(string name)
+            {
+                threadfield = new Thread(func);
+                threadfield.Name = name;
+                threadfield.Start();
+            }
+            //void func()
+            //{
+            //    for (int i = 1; i <= 4; i++)
+            //    {
+            //        Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+            //        //Thread.Sleep(0); 
+            //    }
+            //    Console.WriteLine(Thread.CurrentThread.Name + " завершился");
+            //}
+        }
+        static void Main(string[] args)
+        {
+            Mythread t1 = new Mythread("Thread 1");
+            Mythread t2 = new Mythread("Thread 2");
+            Mythread t3 = new Mythread("Thread 3");
+            Mythread t4 = new Mythread("Thread 4");
+            
+        }
+        static void func()
+        {
+            for (int i = 1; i <= 4; i++)
+            {
+                Console.WriteLine(Thread.CurrentThread.Name + " " + i);
+                //Thread.Sleep(0); 
+            }
+            Console.WriteLine(Thread.CurrentThread.Name + " завершился");
+        }
+    }
+}
